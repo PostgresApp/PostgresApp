67d55b8bb630b616201ccce171e4b74cb1b25aa8
Mattt Thompson <m@mattt.me>
2012-04-24 09:44:45 -0700

    Adding proper header with credits and license to source files

--- postgres-service/main.m
+++ postgres-service/main.m
@@ -1,10 +1,25 @@
+// main.m
 //
-//  main.m
-//  initdb-service
-//
-//  Created by Mattt Thompson on 12/04/05.
-//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
-//
+// Created by Mattt Thompson (http://mattt.me/)
+// Copyright (c) 2012 Heroku (http://heroku.com/)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 #include <xpc/xpc.h>
 #include <Foundation/Foundation.h>
@@ -33,11 +48,7 @@ static void postgres_service_peer_event_handler(xpc_connection_t peer, xpc_objec
             [mutableArguments addObject:[NSString stringWithUTF8String:string]];
             return true;
         });
-        
-//        [NSTask launchedTaskWithLaunchPath:command arguments:mutableArguments];
-//        xpc_object_t reply = xpc_dictionary_create_reply(event);
-//        xpc_connection_send_message(peer, reply);
-        
+
         NSTask *task = [[NSTask alloc] init];
         task.launchPath = command;
         task.arguments = mutableArguments;
@@ -50,7 +61,6 @@ static void postgres_service_peer_event_handler(xpc_connection_t peer, xpc_objec
             xpc_connection_send_message(peer, reply);
         };
         [task launch];
-//        [task waitUntilExit];
 	}
 }
 
