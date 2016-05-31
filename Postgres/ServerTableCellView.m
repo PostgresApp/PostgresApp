a0235444441f6f325dd4e4c440cacae8a92693d1
Chris <chripa@gmx.at>
2016-05-31 16:34:45 +0200

    cleaned up syntax

--- Postgres/ServerTableCellView.m
+++ Postgres/ServerTableCellView.m
@@ -10,25 +10,29 @@
 #import "PostgresServer.h"
 
 
+#define RED [NSColor colorWithRed:252/255.0f green:96/255.0f blue:92/255.0f alpha:1.0f]
+#define GREEN [NSColor colorWithRed:53/255.0f green:202/255.0f blue:74/255.0f alpha:1.0f]
+
+
 @implementation ServerTableCellView
 
 - (void)awakeFromNib {
-	[self addObserver:self forKeyPath:@"objectValue.isRunning" options:NSKeyValueObservingOptionNew context:(void*)self];
+	[self addObserver:self forKeyPath:@"objectValue.isRunning" options:NSKeyValueObservingOptionNew context:nil];
 }
 
 
 -(void)dealloc {
-	[self removeObserver:self forKeyPath:@"objectValue.isRunning" context:(void*)self];
+	[self removeObserver:self forKeyPath:@"objectValue.isRunning"];
 }
 
 
 - (void)drawRect:(NSRect)dirtyRect {
 	[super drawRect:dirtyRect];
 	
-	NSColor *bgColor = ((PostgresServer*)[self objectValue]).isRunning ? [NSColor greenColor] : [NSColor redColor];
-	[bgColor setFill];
+	NSColor *color = ((PostgresServer*)[self objectValue]).isRunning ? GREEN : RED;
+	[color setFill];
 	
-	NSRect rect = NSMakeRect(140, 20, 10, 10);
+	NSRect rect = NSMakeRect(10, 20, 10, 10);
 	NSBezierPath *circlePath = [NSBezierPath bezierPath];
 	[circlePath appendBezierPathWithOvalInRect:rect];
 	[circlePath stroke];
