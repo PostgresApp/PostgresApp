e52baa2591c201a2ce0e68f8a20a04677c15db7d
Chris <chripa@gmx.at>
2016-05-31 16:33:11 +0200

    New add server sheet

---
+++ Postgres/AddServerSheetController.h
@@ -0,0 +1,30 @@
+//
+//  AddServerSheetController.h
+//  Postgres
+//
+//  Created by Chris on 24/05/16.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+#import "PostgresServer.h"
+
+
+@interface AddServerSheetController : NSWindowController
+
+@property NSString *name;
+@property NSString *varPath;
+@property NSUInteger port;
+@property NSArray *versions;
+@property NSUInteger selectedVersionIndex;
+
+@property (readonly) NSString *version;
+@property (readonly) PostgresServer *server;
+
+@property IBOutlet NSPopUpButton *versionsPopup;
+
+- (IBAction)openChooseFolder:(id)sender;
+- (IBAction)ok:(id)sender;
+- (IBAction)cancel:(id)sender;
+
+@end
