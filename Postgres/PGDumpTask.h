7127b3b7aee007a426d14d55c32d89df49e1884c
Chris <chripa@gmx.at>
2016-06-08 14:04:57 +0200

    new task to dump DBs

--- Postgres/PGDumpTask.h
+++ Postgres/PGDumpTask.h
@@ -8,6 +8,17 @@
 
 #import <Foundation/Foundation.h>
 
+typedef void (^PGDumpTaskCompletionHandler)(BOOL success, NSError *error);
+
+@class PostgresServer;
+
 @interface PGDumpTask : NSObject
+
+@property PostgresServer *server;
+@property NSString *dbName;
+@property NSString *filePath;
+
+- (void)startWithCompletionHandler:(PGDumpTaskCompletionHandler)completionBlock;
+- (void)cancel;
 
 @end
--- Postgres/PGDumpTask.m
+++ Postgres/PGDumpTask.m
@@ -7,7 +7,55 @@
 //
 
 #import "PGDumpTask.h"
+#import "PostgresServer.h"
+
+@interface PGDumpTask ()
+@property NSTask *task;
+@end
+
 
 @implementation PGDumpTask
+
+- (void)startWithCompletionHandler:(PGDumpTaskCompletionHandler)completionBlock {
+	dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+		NSError *error = nil;
+		BOOL success = [self executeWithError:&error];
+		if (completionBlock) dispatch_async(dispatch_get_main_queue(), ^{ completionBlock(success, error); });
+	});
+}
+
+
+- (BOOL)executeWithError:(NSError **)error {
+	self.task = [[NSTask alloc] init];
+	self.task.launchPath = [self.server.binPath stringByAppendingPathComponent:@"pg_dump"];
+	self.task.arguments = @[
+							@"-p", @(self.server.port).stringValue,
+							@"-F", @"c",
+							@"-Z", @"9",
+							@"-f", self.filePath,
+							self.dbName
+	];
+	
+	self.task.standardOutput = [[NSPipe alloc] init];
+	self.task.standardError = [[NSPipe alloc] init];
+	[self.task launch];
+	NSString *description = [[NSString alloc] initWithData:[[self.task.standardError fileHandleForReading] readDataToEndOfFile] encoding:NSUTF8StringEncoding];
+	[self.task waitUntilExit];
+	
+	if (self.task.terminationStatus != 0 && error) {
+		NSMutableDictionary *errorUserInfo = [[NSMutableDictionary alloc] init];
+		errorUserInfo[NSLocalizedDescriptionKey] = NSLocalizedString(@"Could not dump database.",nil);
+		if (description) errorUserInfo[NSLocalizedRecoverySuggestionErrorKey] = description;
+		*error = [NSError errorWithDomain:@"com.postgresapp.Postgres.pg_dump" code:self.task.terminationStatus userInfo:errorUserInfo];
+	}
+	
+	return self.task.terminationStatus == 0;
+}
+
+
+- (void)cancel {
+	[self.task terminate];
+	self.task = nil;
+}
 
 @end
