56749145056e7fb2922c1d0ad07f64e08ab2eff2
chrispysoft <chripa@gmx.at>
2016-07-05 20:01:11 +0200

    Created extension for URL and FileManager

--- Postgres/Extensions.swift
+++ Postgres/Extensions.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+
+extension URL {
+	
+	public var isFinderAlias: Bool {
+		get {
+			var isAlias: AnyObject?
+			do {
+				try (self as NSURL).getResourceValue(&isAlias, forKey: .isAliasFileKey)
+			} catch _ {}
+			guard let result = isAlias as! Bool! else { return false }
+			return result
+		}
+	}
+	
+}
+
+
+
+
+
+extension FileManager {
+	
+	public func applicationSupportDirectoryURL(createIfNotExists: Bool) -> URL {
+		let appSupportDirURL = URL.init(string:
+			String(FileManager.default().urlsForDirectory(.applicationSupportDirectory, inDomains: .userDomainMask)[0]).appending(
+				Bundle.main().infoDictionary?[kCFBundleNameKey as String] as! String
+			)
+		)
+		
+		if !FileManager.default().fileExists(atPath: appSupportDirURL!.path!) && createIfNotExists {
+			do {
+				try FileManager.default().createDirectory(at: appSupportDirURL!, withIntermediateDirectories: false, attributes: nil)
+			}
+			catch let error as NSError  {
+				print("Error creating directory: ", error)
+			}
+		}
+		
+		return appSupportDirURL!
+	}
+	
+}
