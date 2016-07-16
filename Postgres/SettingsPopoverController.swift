e1b1a4f259b281ea81c04853e7543100ea5eccde
chrispysoft <chripa@gmx.at>
2016-07-16 14:02:44 +0200

    Changed order of properties

--- Postgres/SettingsPopoverController.swift
+++ Postgres/SettingsPopoverController.swift
@@ -10,31 +10,28 @@ import Cocoa
 
 class SettingsPopoverController: NSViewController, ServerManagerConsumer {
 	
-	dynamic var serverManager: ServerManager!
-	
 	@IBOutlet var serverArrayController: NSArrayController?
 	
+	dynamic var serverManager: ServerManager!
 	
-	@IBAction func openInFinder(_ sender: AnyObject?) {
-		if let varPath = (self.serverArrayController?.selectedObjects.first as? Server)?.varPath {
-			if !NSWorkspace.shared().selectFile(nil, inFileViewerRootedAtPath: varPath) {
-				let userInfo: [String: AnyObject] = [
-					NSLocalizedDescriptionKey: "Folder not found.",
-					NSLocalizedRecoverySuggestionErrorKey: "It will be created the first time you start the server."
-				]
-				let error = NSError(domain: "com.postgresapp.Postgres.missing-folder", code: 0, userInfo: userInfo)
-				self.dismiss(self)
-				NSApp.mainWindow!.windowController?.presentError(error, modalFor: NSApp.mainWindow!, delegate: nil, didPresent: nil, contextInfo: nil)
-			}
+	dynamic var varPathURL: URL? {
+		get {
+			guard let varPath = (self.serverArrayController?.selectedObjects.first as? Server)?.varPath else { return nil }
+			return URL(fileURLWithPath: varPath)
 		}
 	}
 	
 	
-	
-	dynamic var path: URL? {
-		get {
-			guard let p = self.serverArrayController?.selectedObjects.first?.varPath else { return nil }
-			return URL(fileURLWithPath: p)
+	@IBAction func openInFinder(_ sender: AnyObject?) {
+		guard let varPath = (self.serverArrayController?.selectedObjects.first as? Server)?.varPath else { return }
+		if !NSWorkspace.shared().selectFile(nil, inFileViewerRootedAtPath: varPath) {
+			let userInfo: [String: AnyObject] = [
+				NSLocalizedDescriptionKey: "Folder not found.",
+				NSLocalizedRecoverySuggestionErrorKey: "It will be created the first time you start the server."
+			]
+			let error = NSError(domain: "com.postgresapp.Postgres.missing-folder", code: 0, userInfo: userInfo)
+			self.dismiss(self)
+			NSApp.mainWindow!.windowController?.presentError(error, modalFor: NSApp.mainWindow!, delegate: nil, didPresent: nil, contextInfo: nil)
 		}
 	}
 	
