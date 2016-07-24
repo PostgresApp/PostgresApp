54e7702fa0d6c47f4fe4f24d910e6d0bf3cab453
chrispysoft <chripa@gmx.at>
2016-07-24 21:19:05 +0200

    Chaged init and code style

--- Postgres/DatabaseTask.swift
+++ Postgres/DatabaseTask.swift
@@ -16,7 +16,6 @@ class DatabaseTask: NSObject {
 	}
 	
 	private var server: Server
-	private var database: Database?
 	private var task: Task = Task()
 	
 	
@@ -24,16 +23,10 @@ class DatabaseTask: NSObject {
 		self.server = server
 	}
 	
-	init(_ server: Server, _ database: Database) {
-		self.server = server
-		self.database = database
-	}
 	
-	
-	
-	func dump(to file: String, completionHandler: (_: ActionStatus) -> Void) {
+	func dumpDatabase(name: String, to file: String, completionHandler: (_: ActionStatus) -> Void) {
 		DispatchQueue.global().async { 
-			let dumpRes = self.dumpSync(to: file)
+			let dumpRes = self.dumpDatabaseSync(name: name, to: file)
 			DispatchQueue.main.async(execute: {
 				completionHandler(dumpRes)
 			})
@@ -41,22 +34,31 @@ class DatabaseTask: NSObject {
 	}
 	
 	
-	func restore(from file: String, completionHandler: (_: ActionStatus) -> Void) {
-		
+	func restoreDatabase(from file: String, completionHandler: (_: ActionStatus) -> Void) {
 		DispatchQueue.global().async {
 			let dbName = file.lastPathComponent(deletingExtension: true)
-			let createDBRes = self.createDatabaseSync(name: dbName)
+			let createRes = self.createDatabaseSync(name: dbName)
 			
-			switch createDBRes {
+			switch createRes {
 			case .Failure(_):
 				DispatchQueue.main.async {
-					completionHandler(createDBRes)
+					completionHandler(createRes)
 				}
 			case .Success:
-				let restoreRes = self.restoreSync(name: dbName, from: file)
+				let restoreRes = self.restoreDatabaseSync(name: dbName, from: file)
 				DispatchQueue.main.async {
 					completionHandler(restoreRes)
 				}
+			}
+		}
+	}
+	
+	
+	func dropDatabase(name: String, completionHandler: (_: ActionStatus) -> Void) {
+		DispatchQueue.global().async {
+			let dropRes = self.dropDatabaseSync(name: name)
+			DispatchQueue.main.async {
+				completionHandler(dropRes)
 			}
 		}
 	}
@@ -73,14 +75,14 @@ class DatabaseTask: NSObject {
 	/*
 	sync handlers
 	*/
-	private func dumpSync(to file: String) -> ActionStatus {
+	private func dumpDatabaseSync(name: String, to file: String) -> ActionStatus {
 		self.task.launchPath = self.server.binPath.appending("/pg_dump")
 		self.task.arguments = [
 			"-p", String(self.server.port),
 			"-F", "c",
 			"-Z", "9",
 			"-f", file,
-			self.database!.name
+			name
 		]
 		self.task.standardOutput = Pipe()
 		let errorPipe = Pipe()
@@ -129,7 +131,7 @@ class DatabaseTask: NSObject {
 	}
 	
 	
-	private func restoreSync(name: String, from file: String) -> ActionStatus {
+	private func restoreDatabaseSync(name: String, from file: String) -> ActionStatus {
 		self.task.launchPath = self.server.binPath.appending("/pg_restore")
 		self.task.arguments = [
 			"-p", String(self.server.port),
@@ -152,6 +154,32 @@ class DatabaseTask: NSObject {
 				"RawCommandOutput": errorDescription
 			]
 			let error = NSError(domain: "com.postgresapp.Postgres.pg_restore", code: Int(self.task.terminationStatus), userInfo: userInfo)
+			return .Failure(error)
+		}
+	}
+	
+	
+	private func dropDatabaseSync(name: String) -> ActionStatus {
+		self.task.launchPath = self.server.binPath.appending("/dropdb")
+		self.task.arguments = [
+			"-p", String(self.server.port),
+			name
+		]
+		self.task.standardOutput = Pipe()
+		let errorPipe = Pipe()
+		self.task.standardError = errorPipe
+		self.task.launch()
+		let errorDescription = String(data: errorPipe.fileHandleForReading.readDataToEndOfFile(), encoding: .utf8) ?? "(incorrectly encoded error message)"
+		self.task.waitUntilExit()
+		
+		if self.task.terminationStatus == 0 {
+			return .Success
+		} else {
+			let userInfo = [
+				NSLocalizedDescriptionKey: NSLocalizedString("Could not drop database.", comment: ""),
+				"RawCommandOutput": errorDescription
+			]
+			let error = NSError(domain: "com.postgresapp.Postgres.dropdb", code: Int(self.task.terminationStatus), userInfo: userInfo)
 			return .Failure(error)
 		}
 	}
