e4ad5b89cdbe70319f610005656d4082c6bf8c73
chrispysoft <chripa@gmx.at>
2016-08-02 18:15:37 +0200

    Redesigned DatabaseCollectionView

--- Postgres/DatabaseCollectionView.swift
+++ Postgres/DatabaseCollectionView.swift
@@ -1,5 +1,5 @@
 //
-//  DatabaseController.swift
+//  DatabaseCollectionView.swift
 //  Postgres
 //
 //  Created by Chris on 05/07/16.
@@ -8,15 +8,47 @@
 
 import Cocoa
 
+class DatabaseCollectionView: NSCollectionView {
+	
+	override func rightMouseDown(_ event: NSEvent) {
+		let mousePoint = self.convert(event.locationInWindow, from: nil)
+		for i in 0..<self.content.count {
+			let frame = self.frameForItem(at: i)
+			if self.mouse(mousePoint, in: frame) {
+				self.selectionIndexes = [i]
+				break
+			}
+		}
+		
+		super.rightMouseDown(event)
+	}
+	
+	
+	override func menu(for event: NSEvent) -> NSMenu? {
+		let mousePoint = self.convert(event.locationInWindow, from: nil)
+		for i in 0..<self.content.count {
+			let frame = self.frameForItem(at: i)
+			if self.mouse(mousePoint, in: frame) {
+				return self.menu
+			}
+		}
+		return nil
+	}
+	
+}
+
+
 
 class DatabaseItem: NSCollectionViewItem {
 	override var isSelected: Bool {
 		didSet {
-			let v = self.view as? DatabaseItemView
-			v?.selected = isSelected
+			if let v = self.view as? DatabaseItemView {
+				v.selected = isSelected
+			}
 		}
 	}
 }
+
 
 
 class DatabaseItemView: NSView {
@@ -28,44 +60,28 @@ class DatabaseItemView: NSView {
 	
 	
 	override func draw(_ dirtyRect: NSRect) {
-		super.draw(dirtyRect)
-		
-		let offset = CGFloat(10.0)
-		let x = offset
-		let y = offset
-		let w = frame.width - offset*2
-		let h = frame.height - offset*2
-		
-		let rect = CGRect(x: x, y: y, width: w, height: h)
-		let path = NSBezierPath(roundedRect: rect, xRadius: 10, yRadius: 10)
-		
-		(self.selected ? .blue() : .lightGray() as NSColor).setStroke()
-		path.stroke()
+		if self.selected {
+			let offset = CGFloat(10.0)
+			let x = offset
+			let y = offset
+			let w = frame.width - offset*2
+			let h = frame.height - offset*2
+			
+			let rect = CGRect(x: x, y: y, width: w, height: h)
+			let path = NSBezierPath(roundedRect: rect, xRadius: 4, yRadius: 4)
+			NSColor.selectedControlColor().setFill()
+			path.fill()
+		}
 	}
-	
 	
 	
 	override func mouseDown(_ event: NSEvent) {
-		super.mouseDown(event)
-		
 		if event.clickCount == 2 {
 			NSApp.sendAction(#selector(MainViewController.openPsql(_:)), to: nil, from: self)
+		} else {
+			super.mouseDown(event)
 		}
 	}
 	
 	
-	override func rightMouseDown(_ event: NSEvent) {
-		super.rightMouseDown(event)
-		
-		guard let m = self.menu else {
-			Swift.print("\(self.className): IBOutlet menu is nil")
-			return
-		}
-		NSMenu.popUpContextMenu(m, with: event, for: self)
-	}
-	
-	
-	@IBAction func contextMenuAction(_ sender: AnyObject?) {
-		Swift.print("contextMenuAction")
-	}
 }
