6cfd3f9c808aace951c94d8a4929d5dfe2142a2b
chrispysoft <chripa@gmx.at>
2016-08-02 18:25:28 +0200

    Prepared for custom error reporting

--- Postgres/MainWindowController.swift
+++ Postgres/MainWindowController.swift
@@ -41,44 +41,17 @@ class MainWindowController: NSWindowController {
 	
 	override func presentError(_ error: NSError, modalFor window: NSWindow, delegate: AnyObject?, didPresent didPresentSelector: Selector?, contextInfo: UnsafeMutablePointer<Void>?) {
 		print("MainWindowController's presentError() called")
-		super.presentError(error, modalFor: window, delegate: delegate, didPresent: didPresentSelector, contextInfo: contextInfo)
 		
-		//		let alert = NSAlert(error: error)
-		//		alert.beginSheetModal(for: self.view.window!, completionHandler: nil)
-		//
-		//		if let rawCommandOutput = error.userInfo["RawCommandOutput"] as? String {
-		//			let accessoryView = NSTextView(frame: NSMakeRect(0,0,100,100))
-		//			accessoryView.textStorage?.mutableString.setString(rawCommandOutput)
-		//		}
-		//		self.performSegue(withIdentifier: "showError", sender: error)
+		if let rawCommandOutput = error.userInfo["RawCommandOutput"] as? String {
+			// present rawCommandOutput in custom error sheet
+			print("Error with rawCommandOutput detected: \(rawCommandOutput)")
+		} else {
+			super.presentError(error, modalFor: window, delegate: delegate, didPresent: didPresentSelector, contextInfo: contextInfo)
+		}
 	}
 	
-	
-	/*
-	-(void)presentError:(NSError *)error modalForWindow:(NSWindow *)window delegate:(id)delegate didPresentSelector:(SEL)didPresentSelector contextInfo:(void *)contextInfo {
-	NSAlert *alert = [NSAlert alertWithError:error];
-	
-	if (error.userInfo[@"RawCommandOutput"]) {
-	NSArray *tlo;
-	[[NSBundle mainBundle] loadNibNamed:@"AlertAccessoryView" owner:nil topLevelObjects:&tlo];
-	for (id obj in tlo) {
-	if ([obj isKindOfClass:[NSScrollView class]]) {
-	((NSTextView*)((NSScrollView*)obj).contentView.documentView).textStorage.mutableString.string = error.userInfo[@"RawCommandOutput"];
-	alert.accessoryView = obj;
-	}
-	}
-	}
-	
-	[alert beginSheetModalForWindow:window modalDelegate:delegate didEndSelector:didPresentSelector contextInfo:contextInfo];
-	}
-	*/
 	
 	func errorDidPresent(_: AnyObject) {
 		print("errorDidPresent")
 	}
-	
-	
-	//	func prepare(for segue: NSStoryboardSegue, sender: AnyObject?) {
-	//
-	//	}
 }
