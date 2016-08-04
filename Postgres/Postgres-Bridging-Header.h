d3068f7ec6acd59443de47852920e48a64141df2
chrispysoft <chripa@gmx.at>
2016-08-04 15:42:25 +0200

    Changed header import order

--- Postgres/Postgres-Bridging-Header.h
+++ Postgres/Postgres-Bridging-Header.h
@@ -2,5 +2,5 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "ErrorRecoveryAttempter.h"
 #import <libpq-fe.h>
+#import "ErrorRecoveryAttempter.h"
