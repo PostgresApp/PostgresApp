75a2b22ce438a75094454e9af50e1bbe610658c2
chrispysoft <chripa@gmx.at>
2016-08-04 15:04:20 +0200

    Placed FileManager extension to this file

--- Postgres/Extensions.swift
+++ Postgres/Extensions.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+
+extension FileManager {
+	func applicationSupportDirectoryPath(createIfNotExists: Bool) -> String? {
+		guard let url = self.urlsForDirectory(.applicationSupportDirectory, inDomains: .userDomainMask).first else { return nil }
+		let bundleName = Bundle.main().objectForInfoDictionaryKey(kCFBundleNameKey as String) as! String
+		let path = try! url.appendingPathComponent(bundleName).path!
+		
+		if !self.fileExists(atPath: path) && createIfNotExists {
+			do {
+				try self.createDirectory(atPath: path, withIntermediateDirectories: false)
+			}
+			catch {
+				return nil
+			}
+		}
+		
+		return path
+	}
+}
