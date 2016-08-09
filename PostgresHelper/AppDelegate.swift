6e8a17b5ceb8bedaf7fe882571cbd6c6b1a292a6
chrispysoft <chripa@gmx.at>
2016-08-09 16:17:32 +0200

    Renamed icons

--- PostgresHelper/AppDelegate.swift
+++ PostgresHelper/AppDelegate.swift
@@ -6,15 +6,16 @@
 //  Copyright © 2016 postgresapp. All rights reserved.
 //
 
-import Foundation
 import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 	
-	let kAppleInterfaceStyle = "AppleInterfaceStyle"
-	let kAppleInterfaceStyleDark = "Dark"
-	let kAppleInterfaceThemeChangedNotification = "AppleInterfaceThemeChangedNotification" as NSNotification.Name
+	//let serverManager = ServerManager.shared
+	
+	let InterfaceStyle = "AppleInterfaceStyle"
+	let InterfaceStyleDark = "Dark"
+	let InterfaceThemeChangedNotification = "AppleInterfaceThemeChangedNotification" as NSNotification.Name
 	
 	@IBOutlet weak var statusMenu: NSMenu!
 	
@@ -22,29 +23,40 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 	var templateOffImage: NSImage!
 	var templateOnImage: NSImage!
 	var isDarkMode: Bool {
-		return (UserDefaults.standard().object(forKey: kAppleInterfaceStyle) as? String) == kAppleInterfaceStyleDark
+		return (UserDefaults.standard().object(forKey: InterfaceStyle) as? String) == InterfaceStyleDark
 	}
 	
 	
 	func applicationDidFinishLaunching(_ aNotification: Notification) {
-		DistributedNotificationCenter.default().addObserver(forName: kAppleInterfaceThemeChangedNotification, object: nil, queue: OperationQueue.main()) { (notification) in
+		templateOffImage = NSImage(named: "statusicon-off")
+		templateOnImage = NSImage(named: "statusicon-on")
+		templateOffImage.isTemplate = isDarkMode
+		templateOnImage.isTemplate = isDarkMode
+		
+		statusItem = NSStatusBar.system().statusItem(withLength: NSSquareStatusItemLength)
+		statusItem.highlightMode = true
+		statusItem.menu = statusMenu
+		statusItem.image = templateOffImage
+		statusItem.alternateImage = templateOnImage
+		
+		DistributedNotificationCenter.default().addObserver(forName: InterfaceThemeChangedNotification, object: nil, queue: OperationQueue.main()) { (notification) in
 			self.templateOffImage.isTemplate = self.isDarkMode
 			self.templateOnImage.isTemplate = self.isDarkMode
 		}
 		
-		self.templateOffImage = NSImage(named: "status-off")
-		self.templateOnImage = NSImage(named: "status-on")
-		self.templateOffImage.isTemplate = self.isDarkMode
-		self.templateOnImage.isTemplate = self.isDarkMode
-		
-		self.statusItem = NSStatusBar.system().statusItem(withLength: NSSquareStatusItemLength)
-		self.statusItem.highlightMode = true
-		self.statusItem.menu = self.statusMenu
-		self.statusItem.image = self.templateOffImage
-		self.statusItem.alternateImage = self.templateOnImage
+		openPostgresApp(nil)
 	}
 	
 	
+	@IBAction func openPostgresApp(_ sender: AnyObject?) {
+		if !NSWorkspace.shared().launchApplication("/Applicationss/Postgres.app") {
+			let alert = NSAlert()
+			alert.messageText = "Could not launch Postgres.app"
+			alert.informativeText = "Make sure Postgres.app is inside your Applications folder."
+			alert.alertStyle = .critical
+			alert.runModal()
+		}
+	}
 	
 	
 }
