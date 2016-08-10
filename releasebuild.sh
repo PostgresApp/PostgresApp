d40dd4bd3e82a0672f0610a474eedc5a224e5942
Jakob Egger <jakob@eggerapps.at>
2016-08-10 18:57:47 +0200

    Add release build script

---
+++ releasebuild.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Remove existing Postgres.app in Applications folder (since we need that path for building)
+
+rm -Rf /Applications/Postgres.app
+
+
+# Build PostgreSQL, PostGIS, etc.
+
+cd src
+
+make clean
+
+time make
+
+say "I'm finished building Postgres app!"
