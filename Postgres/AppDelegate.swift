c0de4418ca0dbd028cc7049310982c607f9d88c4
chrispysoft <chripa@gmx.at>
2016-08-11 16:37:20 +0200

    Added SMLoginItemSetEnabled

--- Postgres/AppDelegate.swift
+++ Postgres/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import Cocoa
+import ServiceManagement
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
@@ -29,6 +30,16 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 		NotificationCenter.default().addObserver(forName: Server.ChangeNotificationName, object: nil, queue: OperationQueue.main()) { _ in
 			self.serverManager.saveServers()
 		}
+		
+		let registerStatus = LSRegisterURL(try! Bundle.main().bundleURL.appendingPathComponent("Contents/Library/LoginItems/PostgresHelper.app"), true)
+		if registerStatus != noErr {
+			print("Failed to register HelperApp")
+		}
+		let res = SMLoginItemSetEnabled("com.postgresapp.PostgresHelper", true)
+		if !res {
+			print("Failed to launch HelperApp")
+		}
+		
 	}
 	
 	func applicationDidBecomeActive(_ notification: Notification) {
@@ -59,8 +70,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 	
 	
 	@IBAction func updateStatusItem(_ sender: AnyObject?) {
-		print("updateStatusItem")
-		//DistributedNotificationCenter.default().post(name: StatusItemDidChangeNotificationName, object: nil, userInfo: nil)
 		DistributedNotificationCenter.default().postNotificationName(StatusItemDidChangeNotificationName, object: nil, userInfo: nil, deliverImmediately: true)
 	}
 	
