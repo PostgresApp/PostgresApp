360e9ef940996b8677e3383cfab73f8e8af7385c
chrispysoft <chripa@gmx.at>
2016-08-18 17:35:00 +0200

    Added valueTransformer for button image

--- Postgres/BottomBar.swift
+++ Postgres/BottomBar.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2016 postgresapp. All rights reserved.
 //
 
-import Foundation
+import Cocoa
+
+class BottomBarController: NSViewController, MainWindowModelConsumer {
+	
+	dynamic var mainWindowModel: MainWindowModel!
+	
+}
+
+
+
+class SidebarButtonImageTransformer: ValueTransformer {
+	
+	override func transformedValue(_ value: AnyObject?) -> AnyObject? {
+		if value as? Bool == true {
+			return NSImage(imageLiteralResourceName: NSImageNameLeftFacingTriangleTemplate)
+		} else {
+			return NSImage(imageLiteralResourceName: NSImageNameRightFacingTriangleTemplate)
+		}
+	}
+	
+}
