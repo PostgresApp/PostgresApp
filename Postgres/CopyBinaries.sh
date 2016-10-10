1392858f26fab32fb07aa3c1d6ce742828e55a32
chrispysoft <chripa@gmx.at>
2016-10-10 15:04:24 +0200

    Changed comments

--- Postgres/CopyBinaries.sh
+++ Postgres/CopyBinaries.sh
@@ -20,11 +20,11 @@ do
 		mkdir -p "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
 		# copy postgresql binaries
 		cp clusterdb createdb createlang createuser dropdb droplang dropuser ecpg initdb oid2name pg* postgres postmaster psql reindexdb vacuumdb vacuumlo "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
-		#copy proj binaries
+		# copy proj binaries
 		cp cs2cs geod invgeod invproj nad2bin proj "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
-		#copy gdal binaries
+		# copy gdal binaries
 		cp gdal* nearblack ogr2ogr ogrinfo ogrtindex testepsg "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
-		#copy postgis binaries
+		# copy postgis binaries
 		cp raster2pgsql shp2pgsql "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
 
 		# copy dynamic libraries only (no need for static libraries)
@@ -33,12 +33,12 @@ do
 		cp -af *.dylib "${TARGET_VERSIONS_DIR}/${VERSION}/lib/"
 		cp -afR postgresql "${TARGET_VERSIONS_DIR}/${VERSION}/lib/"
 
-		#copy include, share
+		# copy include, share
 		rm -f "${TARGET_VERSIONS_DIR}/${VERSION}/include/json"
 		cp -afR "${SOURCE_VERSIONS_DIR}/${VERSION}/include" "${SOURCE_VERSIONS_DIR}/${VERSION}/share" "${TARGET_VERSIONS_DIR}/${VERSION}/"
 	fi
 done
 
-#create symbolic link
+# create symbolic link
 cd "$BUILT_PRODUCTS_DIR/$CONTENTS_FOLDER_PATH/Versions/"
 ln -sfh ${LATEST_STABLE_PG_VERSION} latest
