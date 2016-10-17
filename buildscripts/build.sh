bb11b14825fdb6b431f68a814e8dd6296f6a4c19
chrispysoft <chripa@gmx.at>
2016-10-17 15:57:40 +0200

    Added build number support and signing for Sparkle

--- buildscripts/build.sh
+++ buildscripts/build.sh
@@ -4,20 +4,19 @@ set -e
 
 PROJECT_ROOT=$(dirname $(pwd))
 
+# get version, buildnumber and increment buildnumber
 VERSION=$(/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" "$PROJECT_ROOT"/Postgres/Info.plist)
-GIT_COMMIT=$(git rev-parse --short HEAD)
+BUILD_NO_NEW=$(($(/usr/libexec/PlistBuddy -c "Print :CFBundleVersion" "$PROJECT_ROOT"/Postgres/Info.plist)+1))
+
+# set incremented build number
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $BUILD_NO_NEW" "$PROJECT_ROOT"/Postgres/Info.plist
 
 PROJECT_FILE="$PROJECT_ROOT"/Postgres.xcodeproj
-ARCHIVE_PATH=~/Documents/Postgres-archives/Postgres-$VERSION-$GIT_COMMIT/Postgres.xcarchive
+ARCHIVE_PATH=~/Documents/Postgres-archives/Postgres-$VERSION-$BUILD_NO_NEW/Postgres.xcarchive
 BGIMG_PATH=background-image/folder_bg.png
-DMG_SRC_PATH=~/Documents/Postgres-archives/Postgres-$VERSION-$GIT_COMMIT/Postgres
-DMG_DST_PATH=~/Documents/Postgres-archives/Postgres-$VERSION-$GIT_COMMIT/Postgres-$VERSION.dmg
-SIGNATURE_PATH=~/Documents/Postgres-archives/Postgres-$VERSION-$GIT_COMMIT/signature.txt
-
-# remove old builds (if exist)
-#[ -e "$ARCHIVE_PATH" ] && rm -r "$ARCHIVE_PATH"
-#[ -e "$DMG_SRC_PATH/Postgres.app" ] && rm -r "$DMG_SRC_PATH/Postgres.app"
-#[ -e "$DMG_DST_PATH" ] && rm "$DMG_DST_PATH"
+DMG_SRC_PATH=~/Documents/Postgres-archives/Postgres-$VERSION-$BUILD_NO_NEW/Postgres
+DMG_DST_PATH=~/Documents/Postgres-archives/Postgres-$VERSION-$BUILD_NO_NEW/Postgres-$VERSION.dmg
+SIGNATURE_PATH=~/Documents/Postgres-archives/Postgres-$VERSION-$BUILD_NO_NEW/signature.txt
 
 # get signing identity
 SIGN_ID=$(security find-certificate -a -c "Developer ID Application" -Z | grep -o -e 'Developer ID [^"]*' | head -n 1)
@@ -41,4 +40,5 @@ vendor/create-dmg-master/create-dmg \
 "$DMG_DST_PATH" \
 "$DMG_SRC_PATH"
 
+# sign update
 ./sign_update "$DMG_DST_PATH" /Volumes/PostgresKey/dsa_priv.pem >"$SIGNATURE_PATH"
