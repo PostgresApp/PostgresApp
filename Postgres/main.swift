389a90390fcebd305b633ed520e66ef52705ecba
chrispysoft <chripa@gmx.at>
2016-10-20 15:30:31 +0200

    Changed startup error messages

--- Postgres/main.swift
+++ Postgres/main.swift
@@ -15,15 +15,14 @@ func checkApplicationPath() {
 	
 	if actualPath != expectedPath {
 		let alert = NSAlert()
-		
-		if !actualPath.hasSuffix("Postgres.app") {
-			alert.messageText = "Postgres.app has been renamed."
-			alert.informativeText = "Please set the name of the app to 'Postgres.app'."
-		} else {
-			alert.messageText = "Postgres.app must be inside your Applications folder."
-			alert.informativeText = "Please move Postgres.app to the Applications folder."
+		if actualPath.hasPrefix("/Applications/") && !actualPath.hasSuffix("Postgres.app") {
+			alert.messageText = "Postgres.app can't be launched."
+			alert.informativeText = "Please change the name of the app back to 'Postgres.app'."
 		}
-		
+		else {
+			alert.messageText = "Postgres.app can't be launched"
+			alert.informativeText = "Please move Postgres.app to your Applications folder.\n\n- use Finder to move the app\n- don't place it in a subfolder\n- don't change the app name"
+		}
 		alert.addButton(withTitle: "OK")
 		alert.runModal()
 		exit(1)
