ff24da24ca1d165aa95c11657f1365156a84f483
chrispysoft <chripa@gmx.at>
2016-10-25 16:40:51 +0200

    Changed preferences controller

--- Postgres/Preferences.swift
+++ Postgres/Preferences.swift
@@ -9,38 +9,9 @@
 import Cocoa
 
 class PreferencesViewController: NSViewController {
-	
-	let preferencesManager = PreferencesManager.shared
-	
-	let clientAppURLs = [
-		URL(fileURLWithPath: "/Applications/Utilities/Terminal.app"),
-		URL(fileURLWithPath: "/Applications/iTerm.app"),
-		URL(fileURLWithPath: "/Applications/Postico.app")
+	dynamic var clientAppNames = [
+		"Terminal",
+		"iTerm",
+		"Postico"
 	]
-	
-	dynamic var clientAppNames: [String] {
-		var result = [String]()
-		for app in clientAppURLs {
-			result.append(app.deletingPathExtension().lastPathComponent)
-		}
-		return result
-	}
-	
-	dynamic var selectionIndex = 0 {
-		didSet {
-			preferencesManager.clientAppURL = clientAppURLs[selectionIndex]
-		}
-	}
-	
-}
-
-
-
-
-class PreferencesManager {
-	
-	static let shared = PreferencesManager()
-	
-	var clientAppURL: URL!
-	
 }
