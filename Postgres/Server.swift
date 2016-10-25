14278e90a2dc265e83f4ceaab726b42c40dadd2f
chrispysoft <chripa@gmx.at>
2016-10-25 13:15:01 +0200

    Removed icon generation in Database class

--- Postgres/Server.swift
+++ Postgres/Server.swift
@@ -528,50 +528,9 @@ class Server: NSObject {
 class Database: NSObject {
 	dynamic var name: String = ""
 	
-	dynamic var icon: NSImage {
-		return NSImage(size: NSSize(width: 64, height: 63), flipped: false) { (dstRect) -> Bool in
-			let baseColor = NSColor(calibratedRed: 0.714, green: 0.823, blue: 0.873, alpha: 1)
-			let frameColor = baseColor.shadow(withLevel: 0.4)
-			let fillColor = baseColor.highlight(withLevel: 0.7)
-			
-			frameColor?.setStroke()
-			fillColor?.setFill()
-			
-			let lineWidth = CGFloat(1.0)
-			
-			for i in 0...3 {
-				var y = lineWidth*0.5
-				if i > 0 {
-					y += (63-lineWidth-8) / 3 * CGFloat(i)
-				}
-				
-				let oval = NSBezierPath(ovalIn: NSRect(x: lineWidth*0.5, y: y, width: 64-lineWidth, height: 8.0))
-				oval.lineWidth = lineWidth
-				oval.stroke()
-				oval.fill()
-				
-				if i < 3 {
-					var y1 = 4+lineWidth*0.5
-					if i > 0 {
-						y1 += (63-lineWidth-8) / 3 * CGFloat(i)
-					}
-					NSRectFillUsingOperation(NSRect(x: lineWidth*0.5, y: y1, width: 64-lineWidth, height: 16.0), NSCompositingOperation.copy)
-				}
-			}
-			
-			frameColor?.setFill()
-			NSRectFillUsingOperation(NSRect(x: 0, y: 4+lineWidth*0.5, width: lineWidth, height: 3*(63-lineWidth-8)/3), NSCompositingOperation.copy)
-			NSRectFillUsingOperation(NSRect(x: 64-lineWidth, y: 4+lineWidth*0.5, width: lineWidth, height: 3*(63-lineWidth-8)/3), NSCompositingOperation.copy)
-			
-			return true
-		}
-	}
-	
-	
 	init(_ name: String) {
 		super.init()
 		self.name = name
 	}
-	
 }
 
