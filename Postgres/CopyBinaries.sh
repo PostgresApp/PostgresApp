e681b33a09c7ef48c00d4a05577541893445ac52
Jakob Egger <jakob@eggerapps.at>
2016-12-15 12:43:11 +0100

    Move a build script to external file

---
+++ Postgres/CopyBinaries.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#  CopyBinaries.sh
+#  Postgres
+#
+#  Created by Jakob Egger on 15/12/16.
+#
+
+ORIG_INSTALL_ROOT="/Applications/Postgres.app/Contents/Versions/${POSTGRES_MAJOR_VERSION}"
+EXECUTABLE_TARGET_DIR="$BUILT_PRODUCTS_DIR/$CONTENTS_FOLDER_PATH/Versions/${POSTGRES_MAJOR_VERSION}"
+
+# copy binaries
+cd "${ORIG_INSTALL_ROOT}/bin/"
+mkdir -p "$EXECUTABLE_TARGET_DIR/bin/"
+# copy postgresql binaries
+cp clusterdb createdb createlang createuser dropdb droplang dropuser ecpg initdb oid2name pg* postgres postmaster psql reindexdb vacuumdb vacuumlo "$EXECUTABLE_TARGET_DIR/bin/"
+#copy proj binaries
+cp cs2cs geod invgeod invproj nad2bin proj "$EXECUTABLE_TARGET_DIR/bin/"
+#copy gdal binaries
+cp gdal* nearblack ogr2ogr ogrinfo ogrtindex testepsg "$EXECUTABLE_TARGET_DIR/bin/"
+#copy postgis binaries
+cp raster2pgsql shp2pgsql "$EXECUTABLE_TARGET_DIR/bin/"
+
+# copy dynamic libraries only (no need for static libraries)
+cd "${ORIG_INSTALL_ROOT}/lib/"
+mkdir -p "$EXECUTABLE_TARGET_DIR/lib/"
+cp -af *.dylib "$EXECUTABLE_TARGET_DIR/lib/"
+cp -afR postgresql "$EXECUTABLE_TARGET_DIR/lib/"
+
+#copy include, share
+rm -f "$EXECUTABLE_TARGET_DIR/include/json"
+cp -afR "${ORIG_INSTALL_ROOT}/include" "${ORIG_INSTALL_ROOT}/share" "$EXECUTABLE_TARGET_DIR/"
+
+#create symbolic link
+cd "$BUILT_PRODUCTS_DIR/$CONTENTS_FOLDER_PATH/Versions/"
+ln -sfh ${POSTGRES_MAJOR_VERSION} latest
