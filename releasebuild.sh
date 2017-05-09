eff2e4fcb59a60fba65e3420c0ebec8abb98f500
Jakob Egger <jakob@eggerapps.at>
2017-05-09 15:52:21 +0200

    Update releasebuild script

--- releasebuild.sh
+++ releasebuild.sh
@@ -2,7 +2,7 @@
 
 # Remove existing Postgres.app in Applications folder (since we need that path for building)
 
-rm -Rf /Applications/Postgres.app
+rm -Rf /Applications/Postgres.app/Contents/Versions/9.6
 
 
 # Build PostgreSQL, PostGIS, etc.
