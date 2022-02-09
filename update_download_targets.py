b8464b1d79333b3c7f25120f050de80da48b5845
Tobias Bussmann <t.bussmann@gmx.net>
2022-02-09 04:04:46 +0100

    update 'download' and 'clean' makefile targets
    extend script to manage 'clean' in addition to the 'download' target
    and run the script

--- update_download_targets.py
+++ update_download_targets.py
@@ -3,22 +3,27 @@ import glob
 for makefile in glob.glob('src*/makefile'):
     print('Processing', makefile)
     download_target = 'download: '
+    clean_target = 'clean: '
     with open(makefile, 'r+') as f:
         lines = f.readlines()
         replace = False
         for i in range(len(lines)):
-            if 'curl' in lines[i].lower() and ':' in lines[i-1]:
+            if not lines[i].strip().startswith('#') and 'curl' in lines[i].lower() and ':' in lines[i-1]:
                 download_target += lines[i-1].partition(':')[0] + ' '
+            if lines[i].lower().startswith('clean-') and ':' in lines[i]:
+                clean_target += lines[i].partition(':')[0] + ' '                
             if 'clean:' in lines[i]:
-                insert_index = i+2
+                download_line = i+2
+                clean_line = i
             if 'check:' in lines[i]:
-                insert_index = i+1
+                download_line = i+1
             if 'download:' in lines[i]:
-                insert_index = i
-                replace = True
-        if replace:
-            lines[insert_index] = download_target+'\n'
+                download_line = i
+                download_replace = True
+        if download_replace:
+            lines[download_line] = download_target+'\n'
         else:
-            lines.insert(insert_index, download_target+'\n')
+            lines.insert(download_line, download_target+'\n')
+        lines[clean_line] = clean_target+'\n'
         f.seek(0)
         f.writelines(lines)
