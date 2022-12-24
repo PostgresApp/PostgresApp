9e13576d356d28c395feb04bec9e2c36c3da74d9
Jakob Egger <jakob@eggerapps.at>
2022-12-24 14:24:40 +0100

    Move setup code to applicationDidFinishLaunching:
    I did this because the main window doesn't appear if I run a modal window before calling NSApplicationMain

--- Postgres/AppDelegate.swift
+++ Postgres/AppDelegate.swift
@@ -19,8 +19,49 @@ class AppDelegate: NSObject, NSApplicationDelegate, SUUpdaterDelegate {
 	@IBOutlet var sparkleUpdater: SUUpdater!
 	@IBOutlet var preferencesMenuItem: NSMenuItem!
 	
+	func checkApplicationPath() {
+		let actualPath = Bundle.main.bundlePath
+		let expectedPath = "/Applications/Postgres.app"
+		
+		if actualPath != expectedPath {
+			let alert = NSAlert()
+			if actualPath.hasPrefix("/Applications/") && !actualPath.hasSuffix("Postgres.app") {
+				alert.messageText = "Postgres.app was renamed"
+				alert.informativeText = "Please change the name of the app back to 'Postgres.app'."
+			}
+			else {
+				alert.messageText = "Postgres.app was not moved to the Applications folder"
+				alert.informativeText = "To ensure that Postgres.app works correctly, please move it to the Applications folder with Finder"
+			}
+			alert.informativeText = alert.informativeText + "\n\nYou can ignore this warning, but some things might not work correctly."
+			alert.addButton(withTitle: "Quit")
+			alert.addButton(withTitle: "Ignore Warning")
+			let response = alert.runModal()
+			if response == .alertFirstButtonReturn {
+				exit(1)
+			}
+		}
+	}
+	
+	func isFirstLaunch() -> Bool {
+		if UserDefaults.standard.bool(forKey: "alreadyLaunched") == false {
+			UserDefaults.standard.set(true, forKey: "alreadyLaunched")
+			return true
+		}
+		return false
+	}
 	
 	func applicationDidFinishLaunching(_ notification: Notification) {
+#if !DEBUG
+		checkApplicationPath()
+#endif
+		ServerManager.shared.loadServers()
+		if isFirstLaunch() {
+			ServerManager.shared.checkForExistingDataDirectories()
+		}
+		ServerManager.shared.createDefaultServer()
+		ServerManager.shared.refreshServerStatuses()
+	
 		NotificationCenter.default.addObserver(forName: Server.PropertyChangedNotification, object: nil, queue: OperationQueue.main) { _ in
 			self.serverManager.saveServers()
 		}
--- Postgres/main.swift
+++ Postgres/main.swift
@@ -10,49 +10,7 @@ import Cocoa
 
 UserDefaults.standard.registerPostgresDefaults()
 
-#if !DEBUG
-func checkApplicationPath() {
-	let actualPath = Bundle.main.bundlePath
-	let expectedPath = "/Applications/Postgres.app"
-	
-	if actualPath != expectedPath {
-		let alert = NSAlert()
-		if actualPath.hasPrefix("/Applications/") && !actualPath.hasSuffix("Postgres.app") {
-			alert.messageText = "Postgres.app was renamed"
-			alert.informativeText = "Please change the name of the app back to 'Postgres.app'."
-		}
-		else {
-			alert.messageText = "Postgres.app was not moved to the Applications folder"
-			alert.informativeText = "To ensure that Postgres.app works correctly, please move it to the Applications folder with Finder"
-		}
-		alert.informativeText = alert.informativeText + "\n\nYou can try to launch Postgres.app anyway, but some things might not work correctly."
-		alert.addButton(withTitle: "Quit")
-		alert.addButton(withTitle: "Launch anyway")
-		let response = alert.runModal()
-		if response == .alertFirstButtonReturn {
-			exit(1)
-		}
-	}
-}
-checkApplicationPath()
-#endif
 
-
-func isFirstLaunch() -> Bool {
-	if UserDefaults.standard.bool(forKey: "alreadyLaunched") == false {
-		UserDefaults.standard.set(true, forKey: "alreadyLaunched")
-		return true
-	}
-	return false
-}
-
-
-ServerManager.shared.loadServers()
-if isFirstLaunch() {
-	ServerManager.shared.checkForExistingDataDirectories()
-}
-ServerManager.shared.createDefaultServer()
-ServerManager.shared.refreshServerStatuses()
 
 
 _ = NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv)
