70d7ad6934658bd9ae9733473ed7b565429c5f8d
Tobias Bussmann <t.bussmann@gmx.net>
2023-02-08 00:45:16 +0100

    update 'download', 'clean' and 'check' makefile targets
    - name the script more generic and make it executable
    - extend script to manage 'check' target
    - remove the feature to insert missing targets
    - fix whitespace at line end
    - run the script

--- update_makefile_targets.py
+++ update_makefile_targets.py
@@ -1,29 +1,36 @@
+#!/usr/bin/env python3
+
 import glob
 
 for makefile in glob.glob('src*/makefile'):
     print('Processing', makefile)
     download_target = 'download: '
+    check_target = 'check: '
     clean_target = 'clean: '
+    download_line = None
+    check_line = None
+    clean_line = None
     with open(makefile, 'r+') as f:
         lines = f.readlines()
-        replace = False
         for i in range(len(lines)):
             if not lines[i].strip().startswith('#') and 'curl' in lines[i].lower() and ':' in lines[i-1]:
                 download_target += lines[i-1].partition(':')[0] + ' '
+            if lines[i].lower().startswith('check-') and ':' in lines[i]:
+                check_target += lines[i].partition(':')[0] + ' '
             if lines[i].lower().startswith('clean-') and ':' in lines[i]:
-                clean_target += lines[i].partition(':')[0] + ' '                
-            if 'clean:' in lines[i]:
-                download_line = i+2
-                clean_line = i
+                clean_target += lines[i].partition(':')[0] + ' '
             if 'check:' in lines[i]:
-                download_line = i+1
+                check_line = i
+            if 'clean:' in lines[i]:
+                clean_line = i
             if 'download:' in lines[i]:
                 download_line = i
-                download_replace = True
-        if download_replace:
-            lines[download_line] = download_target+'\n'
-        else:
-            lines.insert(download_line, download_target+'\n')
-        lines[clean_line] = clean_target+'\n'
+        if download_line:
+            lines[download_line] = download_target.rstrip() + '\n'
+        if check_line:
+            lines[check_line] = check_target.rstrip() + '\n'
+        if clean_line:
+            lines[clean_line] = clean_target.rstrip() + '\n'
         f.seek(0)
         f.writelines(lines)
+        f.truncate()
