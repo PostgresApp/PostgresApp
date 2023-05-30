28b0569cf379e05db6994e430520ba4fd2a48b34
Jakob Egger <jakob@eggerapps.at>
2023-05-30 22:56:11 +0200

    Update PermissionDialog helper

--- PostgresPermissionDialog/main.swift
+++ PostgresPermissionDialog/main.swift
@@ -8,22 +8,19 @@
 
 import AppKit
 
-NSApplication.shared.setActivationPolicy(.accessory)
-NSApplication.shared.activate(ignoringOtherApps: true)
-
-let alert = NSAlert()
 
 var args = CommandLine.arguments
+
 var pid: pid_t?
 while let arg = args.first {
 	args.removeFirst()
-	if arg == "--pid" {
+	if arg == "--client-pid" {
 		pid = pid_t(args.removeFirst())
 	}
 }
 
 guard let pid else {
-	fputs("Usage: \(CommandLine.arguments.first ?? "PostgresPermissionDialog") --pid 12345\n", stderr)
+	fputs("Usage: \(CommandLine.arguments.first ?? "PostgresPermissionDialog") --client-pid 12345\n", stderr)
 	exit(2);
 }
 
@@ -32,16 +29,32 @@ let topLevelProcess = process.getTopLevelProcess()
 
 // check user defaults
 
-UserDefaults.shared.object(forKey: "ClientApplicationPermissions")
+var clientApplicationPermissions: [[String: Any]]
 
+clientApplicationPermissions = UserDefaults.shared.object(forKey: "ClientApplicationPermissions") as? [[String : Any]] ?? []
+
+for client in clientApplicationPermissions {
+	if let path = client["path"] as? String, path == topLevelProcess.path {
+		if let policy = client["policy"] as? String {
+			if policy == "allow" {
+				exit(0)
+			} else {
+				fputs("Connection attempt from \(topLevelProcess.name) denied by Postgres.app settings.\n", stderr)
+				exit(1)
+			}
+		}
+	}
+}
+
+NSApplication.shared.setActivationPolicy(.accessory)
+NSApplication.shared.activate(ignoringOtherApps: true)
+
+let alert = NSAlert()
 
 alert.messageText = "“\(topLevelProcess.name)” wants to connect to Postgres.app"
+alert.informativeText = "You can change your selection later in Postgres.app Settings."
 
 alert.icon = topLevelProcess.icon
-
-let rememberCheckbox = NSButton(checkboxWithTitle: "Remember my choice", target: nil, action: nil)
-
-alert.accessoryView = rememberCheckbox
 
 alert.addButton(withTitle: "Allow")
 alert.addButton(withTitle: "Deny")
@@ -50,8 +63,12 @@ let result = alert.runModal()
 
 switch result {
 case .alertFirstButtonReturn:
+	clientApplicationPermissions.append(["path":topLevelProcess.path, "policy": "allow"])
+	UserDefaults.shared.set(clientApplicationPermissions, forKey: "ClientApplicationPermissions")
 	exit(0)
 default:
 	fputs("The user denied the connection attempt from \(process.path) (\(pid)).\n", stderr)
+	clientApplicationPermissions.append(["path":topLevelProcess.path, "policy": "deny"])
+	UserDefaults.shared.set(clientApplicationPermissions, forKey: "ClientApplicationPermissions")
 	exit(1)
 }
