c8be18d63ede889f91cca270e5f467b5b02bae39
Jakob Egger <jakob@eggerapps.at>
2023-06-01 12:36:54 +0200

    Add alert for unknown processes

--- PostgresPermissionDialog/main.swift
+++ PostgresPermissionDialog/main.swift
@@ -9,14 +9,32 @@
 import AppKit
 
 
+
 var args = CommandLine.arguments
 
 var pid: pid_t?
+var clientAddr: String?
+var clientPort: Int?
+var serverAddr: String?
+var serverPort: Int?
 while let arg = args.first {
 	args.removeFirst()
 	if arg == "--client-pid" {
 		pid = pid_t(args.removeFirst())
 	}
+	if arg == "--client-addr" {
+		clientAddr = args.removeFirst()
+	}
+	if arg == "--client-port" {
+		clientPort = Int(args.removeFirst())
+	}
+	if arg == "--server-addr" {
+		serverAddr = args.removeFirst()
+	}
+	if arg == "--server-port" {
+		serverPort = Int(args.removeFirst())
+	}
+
 }
 
 guard let pid else {
@@ -24,51 +42,92 @@ guard let pid else {
 	exit(2);
 }
 
-let process = try UnixProcessInfo(runningProcessWithPid: pid)
-let topLevelProcess = process.getTopLevelProcess()
+do {
+	let process = try UnixProcessInfo(runningProcessWithPid: pid)
+	let topLevelProcess = process.getTopLevelProcess()
 
-// check user defaults
+	// check user defaults
 
-var clientApplicationPermissions: [[String: Any]]
+	var clientApplicationPermissions: [[String: Any]]
 
-clientApplicationPermissions = UserDefaults.shared.object(forKey: "ClientApplicationPermissions") as? [[String : Any]] ?? []
+	clientApplicationPermissions = UserDefaults.shared.object(forKey: "ClientApplicationPermissions") as? [[String : Any]] ?? []
 
-for client in clientApplicationPermissions {
-	if let path = client["path"] as? String, path == topLevelProcess.path {
-		if let policy = client["policy"] as? String {
-			if policy == "allow" {
-				exit(0)
-			} else {
-				fputs("Connection attempt from \(topLevelProcess.name) denied by Postgres.app settings.\n", stderr)
-				exit(1)
+	for client in clientApplicationPermissions {
+		if let path = client["path"] as? String, path == topLevelProcess.path {
+			if let policy = client["policy"] as? String {
+				if policy == "allow" {
+					exit(0)
+				} else {
+					fputs("Connection attempt from \(topLevelProcess.name) denied by Postgres.app settings.\n", stderr)
+					exit(1)
+				}
 			}
 		}
 	}
+
+	NSApplication.shared.setActivationPolicy(.accessory)
+	NSApplication.shared.activate(ignoringOtherApps: true)
+
+	let alert = NSAlert()
+
+	alert.messageText = "“\(topLevelProcess.name)” wants to connect to Postgres.app"
+	alert.informativeText = "You can change your selection later in Postgres.app Settings."
+
+	alert.icon = topLevelProcess.icon
+
+	alert.addButton(withTitle: "Allow")
+	alert.addButton(withTitle: "Deny")
+
+	let result = alert.runModal()
+
+	switch result {
+	case .alertFirstButtonReturn:
+		clientApplicationPermissions.append(["path":topLevelProcess.path, "policy": "allow"])
+		UserDefaults.shared.set(clientApplicationPermissions, forKey: "ClientApplicationPermissions")
+		exit(0)
+	default:
+		fputs("The user denied the connection attempt from \(process.path) (\(pid)).\n", stderr)
+		clientApplicationPermissions.append(["path":topLevelProcess.path, "policy": "deny"])
+		UserDefaults.shared.set(clientApplicationPermissions, forKey: "ClientApplicationPermissions")
+		exit(1)
+	}
 }
+catch {
+	NSApplication.shared.setActivationPolicy(.accessory)
+	NSApplication.shared.activate(ignoringOtherApps: true)
 
-NSApplication.shared.setActivationPolicy(.accessory)
-NSApplication.shared.activate(ignoringOtherApps: true)
+	let alert = NSAlert()
 
-let alert = NSAlert()
+	alert.messageText = "An unknown process is trying to connect to Postgres.app"
+	var info = ""
+	
+	if let clientAddr, let clientPort {
+		if clientAddr.contains(":") {
+			info += "Client: [\(clientAddr)]:\(clientPort)\n"
+		} else {
+			info += "Client: \(clientAddr):\(clientPort)\n"
+		}
+	}
+	if let serverAddr, let serverPort {
+		if serverAddr.contains(":") {
+			info += "Server: [\(serverAddr)]:\(serverPort)\n"
+		} else {
+			info += "Server: \(serverAddr):\(serverPort)\n"
+		}
+	}
 
-alert.messageText = "“\(topLevelProcess.name)” wants to connect to Postgres.app"
-alert.informativeText = "You can change your selection later in Postgres.app Settings."
+	alert.informativeText = info
+	
+	alert.addButton(withTitle: "Allow")
+	alert.addButton(withTitle: "Deny")
 
-alert.icon = topLevelProcess.icon
+	let result = alert.runModal()
 
-alert.addButton(withTitle: "Allow")
-alert.addButton(withTitle: "Deny")
+	switch result {
+	case .alertFirstButtonReturn:
+		exit(0)
+	default:
+		exit(1)
+	}
 
-let result = alert.runModal()
-
-switch result {
-case .alertFirstButtonReturn:
-	clientApplicationPermissions.append(["path":topLevelProcess.path, "policy": "allow"])
-	UserDefaults.shared.set(clientApplicationPermissions, forKey: "ClientApplicationPermissions")
-	exit(0)
-default:
-	fputs("The user denied the connection attempt from \(process.path) (\(pid)).\n", stderr)
-	clientApplicationPermissions.append(["path":topLevelProcess.path, "policy": "deny"])
-	UserDefaults.shared.set(clientApplicationPermissions, forKey: "ClientApplicationPermissions")
-	exit(1)
 }
