9a8178ba7313a27cc6725302137c4ae5b443c414
Jakob Egger <jakob@eggerapps.at>
2023-09-25 14:52:39 +0200

    Make LoginHelper relocatable

--- PostgresLoginHelper/main.swift
+++ PostgresLoginHelper/main.swift
@@ -8,26 +8,42 @@
 
 import Cocoa
 
-if Bundle.main.bundlePath.hasPrefix("/Applications/Postgres.app") {
-	// Launch the menu bar helper
-	if UserDefaults.shared.bool(forKey: "HideMenuHelperApp") == false {
-		if !NSWorkspace.shared.launchApplication("/Applications/Postgres.app/Contents/MacOS/PostgresMenuHelper.app") {
-			print("Failed to launch MenuHelperApp")
-		}
+if #available(macOS 13, *) {
+	// we only need the else clause
+} else {
+	// on old macOS autostart only works when the app is in /Applications/Postgres.app
+	if !Bundle.main.bundlePath.hasPrefix("/Applications/Postgres.app") {
+		NSLog("Not in /Applications/Postgres.app")
+		exit(0)
 	}
+}
 
-	// Start PostgreSQL servers
-	// This may take a few seconds, so we do this after launching the menu bar helper
-	let serverManager = ServerManager.shared
-	serverManager.loadServers()
-	for server in serverManager.servers {
-		if server.startOnLogin {
-            do {
-                try server.startSync()
-            }
-            catch let error as NSError {
-                Swift.print("Failed to start server \(server.name) because \(error.localizedDescription)")
-            }
+// Launch the menu bar helper
+if UserDefaults.shared.bool(forKey: "HideMenuHelperApp") == false {
+	var containingBundleURL = Bundle.main.bundleURL
+	repeat {
+		containingBundleURL.deleteLastPathComponent()
+	} while containingBundleURL.pathComponents.count > 1 &&  containingBundleURL.pathExtension != "app"
+	if let containingBundle = Bundle(url: containingBundleURL), let menuHelperURL = containingBundle.url(forAuxiliaryExecup[\table: "PostgresMenuHelper.app") {
+		if !NSWorkspace.shared.launchApplication(menuHelperURL.path) {
+			NSLog("Failed to launch PostgresMenuHelper.app")
+		}
+	} else {
+		NSLog("PostgresMenuHelper.app not found")
+	}
+}
+
+// Start PostgreSQL servers
+// This may take a few seconds, so we do this after launching the menu bar helper
+let serverManager = ServerManager.shared
+serverManager.loadServers()
+for server in serverManager.servers {
+	if server.startOnLogin {
+		do {
+			try server.startSync()
+		}
+		catch let error as NSError {
+			Swift.print("Failed to start server \(server.name) because \(error.localizedDescription)")
 		}
 	}
 }
