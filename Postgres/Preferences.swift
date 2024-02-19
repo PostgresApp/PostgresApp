2a126ea15209790db17a307c5c7679c99d7fb20b
Jakob Egger <jakob@eggerapps.at>
2024-02-19 15:51:33 +0100

    Implement client policy selection

--- Postgres/Preferences.swift
+++ Postgres/Preferences.swift
@@ -30,4 +30,26 @@ class PreferencesViewController: NSViewController {
 	@IBAction func resetAppPermissions(_ sender: Any?) {
 		UserDefaults.shared.set(nil, forKey: "ClientApplicationPermissions")
 	}
+	
+	@IBAction func updateAppPolicy(_ sender: Any?) {
+		guard let popupButton = sender as? NSPopUpButton else {
+			NSSound.beep()
+			return
+		}
+		guard let policy = popupButton.titleOfSelectedItem else {
+			NSSound.beep()
+			return
+		}
+		guard let tableView = popupButton.enclosingScrollView?.documentView as? NSTableView else {
+			NSSound.beep()
+			return
+		}
+		let editedRow = tableView.row(for: popupButton)
+		guard var clientApplicationPermissions = UserDefaults.shared.object(forKey: "ClientApplicationPermissions") as? [[String : Any]] else {
+			NSSound.beep()
+			return
+		}
+		clientApplicationPermissions[editedRow]["policy"] = policy
+		UserDefaults.shared.set(clientApplicationPermissions, forKey: "ClientApplicationPermissions")
+	}
 }
