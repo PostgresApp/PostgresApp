f12870cab51bc2814ff46e008588bdcb284ea14b
Jakob Egger <jakob@eggerapps.at>
2025-03-03 12:23:24 +0100

    Extracts binaries directly from ZIP archive
    This changes the CopyBinaries script to extract PostgreSQL binaries from a ZIP archive.
    The downside is, that you can now no longer directly build Postgres.app from another version of Postgres.app.

--- Postgres/CopyBinaries.sh
+++ Postgres/CopyBinaries.sh
@@ -11,145 +11,125 @@ set -o pipefail
 
 TARGET_VERSIONS_DIR="$BUILT_PRODUCTS_DIR/$CONTENTS_FOLDER_PATH/Versions"
 
-cd "$PG_BINARIES_DIR"
-
 for VERSION in ${PG_BINARIES_VERSIONS//_/ }
 do
-	if [ ! -e "${TARGET_VERSIONS_DIR}/${VERSION}" ]
+	if [ -e "${TARGET_VERSIONS_DIR}/${VERSION}" ]
 	then
-		# copy binaries
-		cd "${PG_BINARIES_DIR}/${VERSION}/bin/"
-		mkdir -p "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
-		# copy postgresql binaries
-		cp -a clusterdb createdb createuser dropdb dropuser ecpg initdb oid2name pg_* pgbench postgres psql reindexdb vacuumdb vacuumlo "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
-        if [ -e createlang ]
-        then
-            # removed in PostgreSQL 10
-            cp -a createlang droplang "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
-        fi
-        if [ -e postmaster ]
-        then
-            # removed in PostgreSQL 16
-            cp -a postmaster "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
-        fi
-		# copy proj binaries
-		cp -a cct cs2cs geod invgeod proj invproj "${TARGET_VERSIONS_DIR}/${VERSION}/bin/" || test $IGNORE_MISSING_BINARIES # set env var IGNORE_MISSING_BINARIES=1 to ignore this error
-		if [ -e projinfo ]
-        then
-            # added in proj 6.0.0
-            cp -a projinfo "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
-        fi
-		if [ -e projsync ]
-        then
-            # added in proj 7.0.0
-            cp -a projsync "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
-        fi
-        # copy geos binaries
-        if [ -e geosop ]
-        then
-        	# added in geos 3.10
-            cp -a geosop "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
-        fi
-        # copy gdal binaries
-		cp -a gdal* nearblack ogr* "${TARGET_VERSIONS_DIR}/${VERSION}/bin/" || test $IGNORE_MISSING_BINARIES # set env var IGNORE_MISSING_BINARIES=1 to ignore this error
-        if [ -e testepsg ]
-        then
-			# testepsg was removed in GDAL 3.5, see https://github.com/OSGeo/gdal/pull/3992
-            cp -a testepsg "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
-        fi
-        if [ -e sozip ]
-        then
-			# added in gdal 3.7.0
-            cp -a sozip "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
-        fi
-		# copy postgis binaries
-		cp -a pgsql2shp raster2pgsql shp2pgsql "${TARGET_VERSIONS_DIR}/${VERSION}/bin/" || test $IGNORE_MISSING_BINARIES # set env var IGNORE_MISSING_BINARIES=1 to ignore this error
-        if [ -e pgtopo_export ]
-        then
-            # added in PostGIS 3.3
-            cp -a pgtopo_export pgtopo_import "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
-        fi
-        if [ -e postgis ]
-        then
-            # added in PostGIS 3.4
-            cp -a postgis postgis_restore "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
-        fi        
-        
-		# copy all dynamic libraries
-		cd "${PG_BINARIES_DIR}/${VERSION}/lib/"
-		mkdir -p "${TARGET_VERSIONS_DIR}/${VERSION}/lib/"
-		cp -af *.dylib "${TARGET_VERSIONS_DIR}/${VERSION}/lib/"
-		cp -afR postgresql "${TARGET_VERSIONS_DIR}/${VERSION}/lib/"
-		if [ -e gdalplugins ]
-		then
-			# added in GDAL 3.5
-			cp -af gdalplugins "${TARGET_VERSIONS_DIR}/${VERSION}/lib/"
-		fi 
-		
-		# copy static libraries where a dynamic one doesn't exist
-		for file in *.a
+		rm -r "${TARGET_VERSIONS_DIR}/${VERSION}"
+	fi
+	if [ ! -e "${PG_BINARIES_DIR}/${VERSION}.zip" ]
+	then
+		echo >&2 "Binaries Archive not found. CopyBinaries.sh expected to find a ZIP archive of the binaries at ${PG_BINARIES_DIR}/${VERSION}.zip"
+		exit 1
+	fi
+	
+	
+	# Core PostgreSQL tools
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/clusterdb"
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/createdb"
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/createuser"
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/dropdb"
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/dropuser"
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/ecpg"
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/initdb"
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/oid2name"
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/pg_*"
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/pgbench"
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/postgres"
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/psql"
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/reindexdb"
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/vacuumdb"
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/vacuumlo"
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/createlang" || true # removed in PostgreSQL 10
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/droplang" || true # removed in PostgreSQL 10
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/postmaster" || true # removed in PostgreSQL 16
+	
+	# PostGIS related tools
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/cct" || test $IGNORE_MISSING_BINARIES
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/cs2cs" || test $IGNORE_MISSING_BINARIES
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/geod" || test $IGNORE_MISSING_BINARIES
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/invgeod" || test $IGNORE_MISSING_BINARIES
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/proj" || test $IGNORE_MISSING_BINARIES
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/invproj" || test $IGNORE_MISSING_BINARIES
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/projinfo" || true # added in proj 6.0.0
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/projsync" || true # added in proj 7.0.0
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/geosop" || true # added in geos 3.10
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/gdal*" || test $IGNORE_MISSING_BINARIES
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/nearblack" || test $IGNORE_MISSING_BINARIES
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/ogr*" || test $IGNORE_MISSING_BINARIES
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/testepsg" || true # testepsg was removed in GDAL 3.5, see https://github.com/OSGeo/gdal/pull/3992
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/sozip" || true # added in gdal 3.7.0
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/pgsql2shp" || test $IGNORE_MISSING_BINARIES
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/raster2pgsql" || test $IGNORE_MISSING_BINARIES
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/shp2pgsql" || test $IGNORE_MISSING_BINARIES
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/pgtopo_export" || true # added in PostGIS 3.3
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/pgtopo_import" || true # added in PostGIS 3.3
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/postgis" || true # added in PostGIS 3.4
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/bin/postgis_restore" || true # added in PostGIS 3.4
+	
+	# Dynamic libraries
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/lib/*.dylib" -x "${VERSION}/lib/*/*"
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/lib/postgresql/*"
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/lib/gdalplugins/*" || true # added in GDAL 3.5
+	
+	# Static libraries
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/lib/libpgcommon.a"
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/lib/libpgcommon_shlib.a"
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/lib/libpgfeutils.a"
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/lib/libpgport.a"
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/lib/libpgport_shlib.a"
+	
+	# include, share
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/include/*"
+	unzip "${PG_BINARIES_DIR}/${VERSION}.zip" -d "${TARGET_VERSIONS_DIR}" "${VERSION}/share/*"
+	
+	# make all links within the bundle use @loader_path
+	# if the binaries are signed this will break the code signature
+	# you must make sure to re-sign them afterwards
+	cd "${TARGET_VERSIONS_DIR}/${VERSION}"
+	for file in $(find lib bin -perm +111 -type f)
+	do
+	  if [[ -f $file && ! -L $file ]]
+	  then
+		echo $file
+		otool -L $file | sed '/:$/d;s/ [(].*$//;s/^\s+//' | sort | uniq | while read line
 		do
-            if [[ $file == *LLVM* ]]
-            then
-                continue
-            fi
-			if  [ ! -f "${file%.*}.dylib" ]
-			then
-				cp -af $file "${TARGET_VERSIONS_DIR}/${VERSION}/lib/"
-			fi
-		done
-
-		# make all links within the bundle use @loader_path
-		# if the binaries are signed this will break the code signature
-		# you must make sure to re-sign them afterwards
-		cd "${TARGET_VERSIONS_DIR}/${VERSION}"
-		for file in $(find lib bin -perm +111 -type f)
-		do
-		  if [[ -f $file && ! -L $file ]]
+		  if [[ ! $line == *"$file" && $line == /Applications/Postgres.app/Contents/Versions/* ]]
 		  then
-			echo $file
-			otool -L $file | sed '/:$/d;s/ [(].*$//;s/^\s+//' | sort | uniq | while read line
-			do
-			  if [[ ! $line == *"$file" && $line == /Applications/Postgres.app/Contents/Versions/* ]]
-			  then
-				basename=${line#/Applications/Postgres.app/Contents/Versions/*/lib/}
-				if [[ $file == bin/* ]]
-				then
-				  newname=@loader_path/../lib/$basename
-				elif [[ $file == lib/*/*/*/*/*/*/* ]]
-				then
-				  newname=@loader_path/../../../../../../$basename
-				elif [[ $file == lib/*/*/*/*/*/* ]]
-				then
-				  newname=@loader_path/../../../../../$basename
-				elif [[ $file == lib/*/*/*/*/* ]]
-				then
-				  newname=@loader_path/../../../../$basename
-				elif [[ $file == lib/*/*/*/* ]]
-				then
-				  newname=@loader_path/../../../$basename
-				elif [[ $file == lib/*/*/* ]]
-				then
-				  newname=@loader_path/../../$basename
-				elif [[ $file == lib/*/* ]]
-				then
-				  newname=@loader_path/../$basename
-				elif [[ $file == lib/* ]]
-				then
-				  newname=@loader_path/$basename
-				else
-				  newname=$file
-				fi
-				echo install_name_tool "$file" -change $line $newname
-				install_name_tool "$file" -change $line $newname
-			  fi
-			done
+			basename=${line#/Applications/Postgres.app/Contents/Versions/*/lib/}
+			if [[ $file == bin/* ]]
+			then
+			  newname=@loader_path/../lib/$basename
+			elif [[ $file == lib/*/*/*/*/*/*/* ]]
+			then
+			  newname=@loader_path/../../../../../../$basename
+			elif [[ $file == lib/*/*/*/*/*/* ]]
+			then
+			  newname=@loader_path/../../../../../$basename
+			elif [[ $file == lib/*/*/*/*/* ]]
+			then
+			  newname=@loader_path/../../../../$basename
+			elif [[ $file == lib/*/*/*/* ]]
+			then
+			  newname=@loader_path/../../../$basename
+			elif [[ $file == lib/*/*/* ]]
+			then
+			  newname=@loader_path/../../$basename
+			elif [[ $file == lib/*/* ]]
+			then
+			  newname=@loader_path/../$basename
+			elif [[ $file == lib/* ]]
+			then
+			  newname=@loader_path/$basename
+			else
+			  newname=$file
+			fi
+			echo install_name_tool "$file" -change $line $newname
+			install_name_tool "$file" -change $line $newname
 		  fi
 		done
-		
-		# copy include, share
-		cp -afR "${PG_BINARIES_DIR}/${VERSION}/include" "${PG_BINARIES_DIR}/${VERSION}/share" "${TARGET_VERSIONS_DIR}/${VERSION}/"
-	fi
+	  fi
+	done
 done
 
 # create symbolic link
