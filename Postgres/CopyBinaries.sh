050cdda908b95244832057519fc4b0dd850b78b4
Jakob Egger <jakob@eggerapps.at>
2025-03-03 12:23:24 +0100

    Use rsync to copy binaries
    "cp -a" fails when copying symbolic links to or from shared folders in UTM
    
    We get an error message like the following:
    cp (...): could not copy extended attributes to (...): Too many levels of symbolic links
    
    Using "rsync -a" instead seems to work.

--- Postgres/CopyBinaries.sh
+++ Postgres/CopyBinaries.sh
@@ -21,69 +21,69 @@ do
 		cd "${PG_BINARIES_DIR}/${VERSION}/bin/"
 		mkdir -p "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
 		# copy postgresql binaries
-		cp -a clusterdb createdb createuser dropdb dropuser ecpg initdb oid2name pg_* pgbench postgres psql reindexdb vacuumdb vacuumlo "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
+		rsync -a clusterdb createdb createuser dropdb dropuser ecpg initdb oid2name pg_* pgbench postgres psql reindexdb vacuumdb vacuumlo "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
         if [ -e createlang ]
         then
             # removed in PostgreSQL 10
-            cp -a createlang droplang "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
+            rsync -a createlang droplang "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
         fi
         if [ -e postmaster ]
         then
             # removed in PostgreSQL 16
-            cp -a postmaster "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
+            rsync -a postmaster "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
         fi
 		# copy proj binaries
-		cp -a cct cs2cs geod invgeod proj invproj "${TARGET_VERSIONS_DIR}/${VERSION}/bin/" || test $IGNORE_MISSING_BINARIES # set env var IGNORE_MISSING_BINARIES=1 to ignore this error
+		rsync -a cct cs2cs geod invgeod proj invproj "${TARGET_VERSIONS_DIR}/${VERSION}/bin/" || test $IGNORE_MISSING_BINARIES # set env var IGNORE_MISSING_BINARIES=1 to ignore this error
 		if [ -e projinfo ]
         then
             # added in proj 6.0.0
-            cp -a projinfo "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
+            rsync -a projinfo "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
         fi
 		if [ -e projsync ]
         then
             # added in proj 7.0.0
-            cp -a projsync "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
+            rsync -a projsync "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
         fi
         # copy geos binaries
         if [ -e geosop ]
         then
         	# added in geos 3.10
-            cp -a geosop "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
+            rsync -a geosop "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
         fi
         # copy gdal binaries
-		cp -a gdal* nearblack ogr* "${TARGET_VERSIONS_DIR}/${VERSION}/bin/" || test $IGNORE_MISSING_BINARIES # set env var IGNORE_MISSING_BINARIES=1 to ignore this error
+		rsync -a gdal* nearblack ogr* "${TARGET_VERSIONS_DIR}/${VERSION}/bin/" || test $IGNORE_MISSING_BINARIES # set env var IGNORE_MISSING_BINARIES=1 to ignore this error
         if [ -e testepsg ]
         then
 			# testepsg was removed in GDAL 3.5, see https://github.com/OSGeo/gdal/pull/3992
-            cp -a testepsg "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
+            rsync -a testepsg "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
         fi
         if [ -e sozip ]
         then
 			# added in gdal 3.7.0
-            cp -a sozip "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
+            rsync -a sozip "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
         fi
 		# copy postgis binaries
-		cp -a pgsql2shp raster2pgsql shp2pgsql "${TARGET_VERSIONS_DIR}/${VERSION}/bin/" || test $IGNORE_MISSING_BINARIES # set env var IGNORE_MISSING_BINARIES=1 to ignore this error
+		rsync -a pgsql2shp raster2pgsql shp2pgsql "${TARGET_VERSIONS_DIR}/${VERSION}/bin/" || test $IGNORE_MISSING_BINARIES # set env var IGNORE_MISSING_BINARIES=1 to ignore this error
         if [ -e pgtopo_export ]
         then
             # added in PostGIS 3.3
-            cp -a pgtopo_export pgtopo_import "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
+            rsync -a pgtopo_export pgtopo_import "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
         fi
         if [ -e postgis ]
         then
             # added in PostGIS 3.4
-            cp -a postgis postgis_restore "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
+            rsync -a postgis postgis_restore "${TARGET_VERSIONS_DIR}/${VERSION}/bin/"
         fi        
         
 		# copy all dynamic libraries
 		cd "${PG_BINARIES_DIR}/${VERSION}/lib/"
 		mkdir -p "${TARGET_VERSIONS_DIR}/${VERSION}/lib/"
-		cp -af *.dylib "${TARGET_VERSIONS_DIR}/${VERSION}/lib/"
-		cp -afR postgresql "${TARGET_VERSIONS_DIR}/${VERSION}/lib/"
+		rsync -a *.dylib "${TARGET_VERSIONS_DIR}/${VERSION}/lib/"
+		rsync -a postgresql "${TARGET_VERSIONS_DIR}/${VERSION}/lib/"
 		if [ -e gdalplugins ]
 		then
 			# added in GDAL 3.5
-			cp -af gdalplugins "${TARGET_VERSIONS_DIR}/${VERSION}/lib/"
+			rsync -a gdalplugins "${TARGET_VERSIONS_DIR}/${VERSION}/lib/"
 		fi 
 		
 		# copy static libraries where a dynamic one doesn't exist
@@ -95,7 +95,7 @@ do
             fi
 			if  [ ! -f "${file%.*}.dylib" ]
 			then
-				cp -af $file "${TARGET_VERSIONS_DIR}/${VERSION}/lib/"
+				rsync -a $file "${TARGET_VERSIONS_DIR}/${VERSION}/lib/"
 			fi
 		done
 
@@ -148,7 +148,7 @@ do
 		done
 		
 		# copy include, share
-		cp -afR "${PG_BINARIES_DIR}/${VERSION}/include" "${PG_BINARIES_DIR}/${VERSION}/share" "${TARGET_VERSIONS_DIR}/${VERSION}/"
+		rsync -a "${PG_BINARIES_DIR}/${VERSION}/include" "${PG_BINARIES_DIR}/${VERSION}/share" "${TARGET_VERSIONS_DIR}/${VERSION}/"
 	fi
 done
 
