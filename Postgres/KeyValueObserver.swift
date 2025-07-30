fba197f1bca4aed0dd94267a65250f6be7e1f3b1
Jakob Egger <jakob@eggerapps.at>
2025-07-30 12:41:15 +0200

    Fix for KVO crash on macOS 12
    This is the first crash we found thanks to our new crash reporter.
    (the crash was caused because we did not unregister a KVO observer. Newer versions of macOS might do that automatically)

--- Postgres/KeyValueObserver.swift
+++ Postgres/KeyValueObserver.swift
@@ -15,22 +15,28 @@ class KeyValueObserver: NSObject {
 	let keyPath: String
 	private let callback: KVOCallback
 	
-	init(_ keyPath: String, _ callback: @escaping KVOCallback) {
+	init(_ keyPath: String, _ callback: @escaping KVOCallback, _ object: NSObject) {
 		self.keyPath = keyPath
 		self.callback = callback
+		self.object = object
 	}
 	
 	override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
 		callback(change)
 	}
 	
+	weak var object: NSObject?
+	
+	deinit {
+		object?.removeObserver(self, forKeyPath: keyPath)
+	}
 }
 
 
 
 extension NSObject {
 	func observe(_ keyPath: String, options: NSKeyValueObservingOptions = [], callback: @escaping KeyValueObserver.KVOCallback) -> KeyValueObserver {
-		let observer = KeyValueObserver(keyPath, callback)
+		let observer = KeyValueObserver(keyPath, callback, self)
 		self.addObserver(observer, forKeyPath: keyPath, options: options, context: nil)
 		return observer
 	}
