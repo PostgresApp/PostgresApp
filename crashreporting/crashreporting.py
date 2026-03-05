2fad1f59fec3fe31d11448d3bd2de2401b4c9058
Jakob Egger <jakob@eggerapps.at>
2026-03-05 13:47:13 +0100

    Crash Reporting: Log query string arguments

--- crashreporting/crashreporting.py
+++ crashreporting/crashreporting.py
@@ -16,10 +16,14 @@ class CrashReporting(tornado.web.RequestHandler):
 		# we can extract Postgres.app version from the user agent
 		user_agent = self.request.headers.get("User-Agent")
 		
+		# parse all the query arguments into a dictionary
+		query = {k: self.get_argument(k) for k in self.request.query_arguments}
+		
 		await self.application.pg_pool.execute(
-			"INSERT INTO crash_reports(report, user_agent) VALUES($1, $2);",
+			"INSERT INTO crash_reports(report, user_agent, query) VALUES($1, $2, $3);",
 			crash_report,
-			user_agent
+			user_agent,
+			json.dumps(query)
 		)
 		
 		self.finish(f"Crash Report Received!")
